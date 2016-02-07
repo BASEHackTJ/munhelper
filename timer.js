7ec97ea78418ffd5b5ab4d9dc907875a65339db5
EmersonB <eberlik@gmail.com>
2016-02-06 22:15:41 -0500

    Started adding main.html

--- timer.js
+++ timer.js
@@ -1,7 +1,11 @@
 function countdown( elementName, minutes, seconds )
 {
     var element, endTime, hours, mins, msLeft, time;
-
+    console.log("check1");
+    console.log("elementName = " + elementName);
+    element = document.getElementById( elementName );
+    console.log("element = " + element);
+    console.log("check2");
     function twoDigits( n )
     {
         return (n <= 9 ? "0" + n : n);
@@ -11,17 +15,16 @@ function countdown( elementName, minutes, seconds )
     {
         msLeft = endTime - (+new Date);
         if ( msLeft < 1000 ) {
-            element.innerHTML = "DONE!";
+            element.innerHTML = "countdown's over!";
         } else {
             time = new Date( msLeft );
             hours = time.getUTCHours();
             mins = time.getUTCMinutes();
-            element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+             element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
             setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
         }
     }
 
-    element = document.getElementById( elementName );
     endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
     updateTimer();
 }
